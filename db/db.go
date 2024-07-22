36d17751ca63206446e72a03184cf5e0bd8bd7fd
Sean Dougherty <sd@kexp.org>
2024-07-22 08:58:08 -1000

    user table is now created. event table uses the user_id as a foreign key for the event creation user. stemmed route for signup

--- db/db.go
+++ db/db.go
@@ -10,7 +10,7 @@ var DB *sql.DB
 var err error
 
 func InitDB() {
-	DB, err = sql.Open("sqlite", "api.db")
+	DB, err = sql.Open("sqlite", "api.db?_foreign_keys=on") //force foreign key as it's off by default in sqlite3
 
 	if err != nil {
 		panic("DB Connection Failed")
@@ -21,6 +21,19 @@ func InitDB() {
 }
 
 func CreateTables() {
+	createUsersTable := `
+		CREATE TABLE IF NOT EXISTS users (
+    	id INTEGER PRIMARY KEY AUTOINCREMENT,
+    	email TEXT NOT NULL UNIQUE,
+    	password TEXT NOT NULL
+)`
+
+	_, err = DB.Exec(createUsersTable)
+
+	if err != nil {
+		panic("Could not create users table")
+	}
+
 	createEventsTable := `
 	    CREATE TABLE IF NOT EXISTS events (
 	    id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,7 +41,8 @@ func CreateTables() {
 	    description TEXT NOT NULL,
 	    location TEXT NOT NULL,
 	    date_time DATETIME NOT NULL,
-	    user_id INTEGER NOT NULL
+	    user_id INTEGER NOT NULL,
+	    FOREIGN KEY(user_id) REFERENCES users(id)
 	)`
 
 	_, err = DB.Exec(createEventsTable)
